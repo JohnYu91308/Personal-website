c3aa9b4c617d851a0693cdfc33c470c2f3773bbd
John <johnyu91308@gmail.com>
2024-07-09 22:08:31 +0800

    send email completed

--- main.js
+++ main.js
@@ -1,34 +1,59 @@
 // SHOW MENU
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 // MENU SHOW
-if(navToggle){
-        navToggle.addEventListener('click', () =>{
-            navMenu.classList.add('show-menu')
-        })
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
 }
 
 //MENU HIIDEN
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
 
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click',linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // SHADOW HEADER
-const shadowHeader = () =>{
+const shadowHeader = () => {
     const header = document.getElementById('header')
     this.scrollY >= 50 ? header.classList.add('shadow-header')
-                       : header.classList.remove('shadow-header')
+        : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
+
+// Email JS
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+// serviceID templateID #form publicKey
+    emailjs.sendForm('service_zzfn0y6','template_6v4zeqf','#contact-form','fqNJfTwjSkhr-L4VZ')
+    .then(() =>{
+        contactMessage.textContent = '傳送成功！'
+
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset()
+    }, () => {
+        contactMessage.textContent = '傳送失敗！'
+    })
+
+}
+
+contactForm.addEventListener('submit', sendEmail)
