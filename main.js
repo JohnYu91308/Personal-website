cb0d995c6ffe69c96ca488b90a3e149b602fea5a
John <johnyu91308@gmail.com>
2024-07-12 22:27:12 +0800

    Dark mode completed

--- main.js
+++ main.js
@@ -35,36 +35,88 @@ window.addEventListener('scroll', shadowHeader)
 
 // Email JS
 const contactForm = document.getElementById('contact-form'),
-      contactMessage = document.getElementById('contact-message')
+    contactMessage = document.getElementById('contact-message')
 
 const sendEmail = (e) => {
     e.preventDefault()
 
-// serviceID templateID #form publicKey
-    emailjs.sendForm('service_zzfn0y6','template_6v4zeqf','#contact-form','fqNJfTwjSkhr-L4VZ')
-    .then(() =>{
-        contactMessage.textContent = '傳送成功！'
+    // serviceID templateID #form publicKey
+    emailjs.sendForm('service_zzfn0y6', 'template_6v4zeqf', '#contact-form', 'fqNJfTwjSkhr-L4VZ')
+        .then(() => {
+            contactMessage.textContent = '傳送成功！'
 
-        setTimeout(() => {
-            contactMessage.textContent = ''
-        }, 5000)
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
 
-        contactForm.reset()
-    }, () => {
-        contactMessage.textContent = '傳送失敗！'
-    })
+            contactForm.reset()
+        }, () => {
+            contactMessage.textContent = '傳送失敗！'
+        })
 
 }
 
 contactForm.addEventListener('submit', sendEmail)
 
 // Show Scroll Up
-const scrollup = () => { 
+const scrollup = () => {
     const scrollUp = document.getElementById('scroll-up')
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class 
     this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
         : scrollUp.classList.remove('show-scroll')
-    }
-    window.addEventListener('scroll', scrollup)
+}
+window.addEventListener('scroll', scrollup)
 
+
+// Scroll Section Active Link    
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight, 
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// Dark Light Theme
+const themeButton = document.getElementById('theme-button') 
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class 
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light' 
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-contrast-2-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+// If the validation is fulfilled, we ask what the issue was to know if we activated or dec 
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'] (darkTheme) 
+    themeButton.classList[selectedIcon === 'ri-contrast-2-line' ? 'add' : 'remove'](iconTheme)
+}
+
+//Active /deactive the theme manually with the button
+themeButton.addEventListener('click', () => {
+
+    // Add or remove the dark / icon theme 
+    document.body.classList.toggle(darkTheme) 
+    themeButton.classList.toggle(iconTheme)
+
+    // We save the theme and the current icon that the user chose 
+    localStorage.setItem('selected-theme', getCurrentTheme()) 
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
     
